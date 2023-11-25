cd781624621e9667fc2cccdde139a50080228b8e
flyzero <fly.zero@foxmail.com>
2023-11-25 11:33:57 +0800

    style: 统一使用空格缩进

--- src/w1_therm/sqlite_storage.cpp
+++ src/w1_therm/sqlite_storage.cpp
@@ -18,90 +18,90 @@
 
 sqlite_storage::sqlite_storage(const char * path)
 {
-	assert(path);
+    assert(path);
 
-	sqlite3 * handle{ nullptr };
-	if (sqlite3_open(path, &handle) != SQLITE_OK)
-	{
-		sqlite3_close(handle);
-		throw std::runtime_error{ "Cannot initialize SQLite" };
-	}
+    sqlite3 * handle{ nullptr };
+    if (sqlite3_open(path, &handle) != SQLITE_OK)
+    {
+        sqlite3_close(handle);
+        throw std::runtime_error{ "Cannot initialize SQLite" };
+    }
 
-	auto const sql =
-		"create table if not exists tb_therm("
-			"id    integer  primary key autoincrement,"
-			"name  text(64) not null,"
-			"therm integer  not null,"
-			"time  integer  not null"
-		")";
+    auto const sql =
+        "create table if not exists tb_therm("
+            "id    integer  primary key autoincrement,"
+            "name  text(64) not null,"
+            "therm integer  not null,"
+            "time  integer  not null"
+        ")";
 
-	char * errmsg{ nullptr };
-	auto const err = sqlite3_exec(handle, sql, nullptr, nullptr, &errmsg);
-	if (err != SQLITE_OK)
-	{
-		sqlite3_close(handle);
-		throw std::runtime_error{ "Cannot create SQLite table" };
-	}
+    char * errmsg{ nullptr };
+    auto const err = sqlite3_exec(handle, sql, nullptr, nullptr, &errmsg);
+    if (err != SQLITE_OK)
+    {
+        sqlite3_close(handle);
+        throw std::runtime_error{ "Cannot create SQLite table" };
+    }
 
-	db_.reset(handle);
+    db_.reset(handle);
 }
 
 sqlite_storage::~sqlite_storage()
 {
-	sqlite3_close(db_.release());
+    sqlite3_close(db_.release());
 }
 
 void sqlite_storage::insert(const char * name, const double value, time_t now)
 {
-	char sql[256];
-	auto const len = snprintf(sql, sizeof sql,
-		"insert into tb_therm (name,therm,time) values ('%s',%f,%lu)", name, value, now);
-	assert(len > 0 && static_cast<size_t>(len) < sizeof sql);
-	(void)len;
+    char sql[256];
+    auto const len = snprintf(sql, sizeof sql,
+        "insert into tb_therm (name,therm,time) values ('%s',%f,%lu)", name, value, now);
+    assert(len > 0 && static_cast<size_t>(len) < sizeof sql);
+    (void)len;
 
-	char * errmsg;
-	auto const err = sqlite3_exec(db_.get(), sql, nullptr, nullptr, &errmsg);
-	if unlikely(err != SQLITE_OK)
-	{
-		throw std::runtime_error{ "Cannot insert record: " + std::string{ errmsg } };
-		sqlite3_free(errmsg);
-	}
-	else
-	{
+    char * errmsg;
+    auto const err = sqlite3_exec(db_.get(), sql, nullptr, nullptr, &errmsg);
+    if unlikely(err != SQLITE_OK)
+    {
+        throw std::runtime_error{ "Cannot insert record: " + std::string{ errmsg } };
+        sqlite3_free(errmsg);
+    }
+    else
+    {
 #ifdef _DEBUG_
-		std::cerr << "new record: " << name << ',' << value << ',' << now << std::endl;
+        std::cerr << "new record: " << name << ',' << value << ',' << now << std::endl;
 #endif
-	}
+    }
 }
 
 void sqlite_storage::select(size_t count, int (*callback)(void*,int,char**,char**), void * user)
 {
-	char sql[256];
-	auto const len = snprintf(sql, sizeof sql, "select * from tb_therm limit %zu", count);
-	assert(len > 0 && static_cast<size_t>(len) < sizeof sql);
-	(void)len;
+    char sql[256];
+    auto const len = snprintf(sql, sizeof sql, "select * from tb_therm limit %zu", count);
+    assert(len > 0 && static_cast<size_t>(len) < sizeof sql);
+    (void)len;
 
-	char * errmsg;
-	auto const err = sqlite3_exec(db_.get(), "select * from tb_therm", callback, user, &errmsg);
-	if unlikely(err != SQLITE_OK)
-	{
-		throw std::runtime_error{ "Cannot select records: " + std::string{ errmsg } };
-		sqlite3_free(errmsg);
-	}
+    char * errmsg;
+    auto const err = sqlite3_exec(db_.get(), "select * from tb_therm", callback, user, &errmsg);
+    if unlikely(err != SQLITE_OK)
+    {
+        throw std::runtime_error{ "Cannot select records: " + std::string{ errmsg } };
+        sqlite3_free(errmsg);
+    }
 }
 
 void sqlite_storage::delete_where_id_not_greater_than(size_t id)
 {
-	char sql[256];
-	auto const len = snprintf(sql, sizeof sql, "delete from tb_therm where id <= %zu", id);
-	assert(len > 0 && static_cast<size_t>(len) < sizeof sql);
-	(void)len;
+    char sql[256];
+    auto const len = snprintf(sql, sizeof sql, "delete from tb_therm where id <= %zu", id);
+    assert(len > 0 && static_cast<size_t>(len) < sizeof sql);
+    (void)len;
 
-	char * errmsg;
-	auto const err = sqlite3_exec(db_.get(), sql, nullptr, nullptr, &errmsg);
-	if unlikely(err != SQLITE_OK)
-	{
-		throw std::runtime_error{ "Cannot delete records: " + std::string{ errmsg } };
-		sqlite3_free(errmsg);
-	}
+    char * errmsg;
+    auto const err = sqlite3_exec(db_.get(), sql, nullptr, nullptr, &errmsg);
+    if unlikely(err != SQLITE_OK)
+    {
+        throw std::runtime_error{ "Cannot delete records: " + std::string{ errmsg } };
+        sqlite3_free(errmsg);
+    }
 }
--- src/w1_therm/w1_therm.cpp
+++ src/w1_therm/w1_therm.cpp
@@ -39,7 +39,7 @@
  */
 struct sqlite_config
 {
-	std::string path_{ }; ///< path to sqlite database
+    std::string path_{ }; ///< path to sqlite database
 };
 
 /**
@@ -47,10 +47,10 @@ struct sqlite_config
  */
 struct influx_config
 {
-	std::string host_{ };   ///< host of influxdb
-	std::string org_{ };    ///< organization of influxdb
-	std::string bucket_{ }; ///< bucket of influxdb
-	std::string token_{ };  ///< token of influxdb
+    std::string host_{ };   ///< host of influxdb
+    std::string org_{ };    ///< organization of influxdb
+    std::string bucket_{ }; ///< bucket of influxdb
+    std::string token_{ };  ///< token of influxdb
 };
 
 /**
@@ -58,355 +58,355 @@ struct influx_config
  */
 struct therm_config
 {
-	std::string   w1_slave_path_{ };    ///< path to w1_slave
-	std::string   senor_name_{ };       ///< name of senor
-	bool          daemonlize_{ false }; ///< daemonlize if set
-	sqlite_config sqlite_db_{ };        ///< config for sqlite database
-	influx_config influx_db_{ };        ///< config for influx database
+    std::string   w1_slave_path_{ };    ///< path to w1_slave
+    std::string   senor_name_{ };       ///< name of senor
+    bool          daemonlize_{ false }; ///< daemonlize if set
+    sqlite_config sqlite_db_{ };        ///< config for sqlite database
+    influx_config influx_db_{ };        ///< config for influx database
 };
 
 struct storage_t
 {
-	storage_t(sqlite_storage && sqlite, influx_storage && influx)
-		: sqlite_{ std::move(sqlite) }
-		, influx_{ std::move(influx) }
-	{ }
+    storage_t(sqlite_storage && sqlite, influx_storage && influx)
+        : sqlite_{ std::move(sqlite) }
+        , influx_{ std::move(influx) }
+    { }
 
-	void insert(const char * name, double value, time_t now);
+    void insert(const char * name, double value, time_t now);
 
-	size_t sqlite_count_{0}; ///< 执行 sqlite 插入的次数，不代表 sqlite 中的记录数
-	sqlite_storage sqlite_;
-	influx_storage influx_;
+    size_t sqlite_count_{0}; ///< 执行 sqlite 插入的次数，不代表 sqlite 中的记录数
+    sqlite_storage sqlite_;
+    influx_storage influx_;
 };
 
 static auto s_running = false;
 
 void storage_t::insert(const char * name, double value, time_t now)
 {
-	if (sqlite_count_ == 0) try
-	{
-		influx_.insert(name, value, now);
-		return;
-	}
-	catch (std::exception const & e)
-	{
-		syslog(LOG_USER | LOG_ERR, "Cannot insert to influxdb: %s\n", e.what());
-	}
+    if (sqlite_count_ == 0) try
+    {
+        influx_.insert(name, value, now);
+        return;
+    }
+    catch (std::exception const & e)
+    {
+        syslog(LOG_USER | LOG_ERR, "Cannot insert to influxdb: %s\n", e.what());
+    }
 
-	++sqlite_count_;
-	try
-	{
-		sqlite_.insert(name, value, now);
-	}
-	catch (std::exception const & e)
-	{
-		syslog(LOG_USER | LOG_ERR, "Cannot insert to sqlite: %s\n", e.what());
-	}
+    ++sqlite_count_;
+    try
+    {
+        sqlite_.insert(name, value, now);
+    }
+    catch (std::exception const & e)
+    {
+        syslog(LOG_USER | LOG_ERR, "Cannot insert to sqlite: %s\n", e.what());
+    }
 
-	if ((sqlite_count_ % 10) == 0 && influx_.is_bucket_exists()) while (true)
-	{
-		using name_t = boost::static_strings::static_string<64>;
-		using record_t = std::tuple<long, double, time_t, name_t>;
-		using vector_t = boost::container::static_vector<record_t, 100>;
-		vector_t records;
+    if ((sqlite_count_ % 10) == 0 && influx_.is_bucket_exists()) while (true)
+    {
+        using name_t = boost::static_strings::static_string<64>;
+        using record_t = std::tuple<long, double, time_t, name_t>;
+        using vector_t = boost::container::static_vector<record_t, 100>;
+        vector_t records;
 
-		auto const callback = [](void * user, int argc, char ** argv, char ** col) -> int
-		{
-			assert(argc == 4);
-			assert(col[0] == std::string_view{ "id" });
-			assert(col[1] == std::string_view{ "name" });
-			assert(col[2] == std::string_view{ "therm" });
-			assert(col[3] == std::string_view{ "time" });
-			auto const id = std::atol(argv[0]);
-			auto const name = argv[1];
-			auto const value = std::atof(argv[2]);
-			auto const now = std::atol(argv[3]);
-			auto & records = *static_cast<vector_t *>(user);
-			records.emplace_back(id, value, now, name);
-			return 0;
-		};
+        auto const callback = [](void * user, int argc, char ** argv, char ** col) -> int
+        {
+            assert(argc == 4);
+            assert(col[0] == std::string_view{ "id" });
+            assert(col[1] == std::string_view{ "name" });
+            assert(col[2] == std::string_view{ "therm" });
+            assert(col[3] == std::string_view{ "time" });
+            auto const id = std::atol(argv[0]);
+            auto const name = argv[1];
+            auto const value = std::atof(argv[2]);
+            auto const now = std::atol(argv[3]);
+            auto & records = *static_cast<vector_t *>(user);
+            records.emplace_back(id, value, now, name);
+            return 0;
+        };
 
-		try
-		{
-			sqlite_.select(records.capacity(), callback, &records);
-		}
-		catch (std::exception const & e)
-		{
-			syslog(LOG_USER | LOG_ERR, "Cannot select from sqlite: %s\n", e.what());
-			break;
-		}
+        try
+        {
+            sqlite_.select(records.capacity(), callback, &records);
+        }
+        catch (std::exception const & e)
+        {
+            syslog(LOG_USER | LOG_ERR, "Cannot select from sqlite: %s\n", e.what());
+            break;
+        }
 
-		auto i = 0;
-		for (auto const & [id, value, now, name] : records) try
-		{
-			influx_.insert(name.data(), value, now);
-			++i;
-		}
-		catch (std::exception const & e)
-		{
-			syslog(LOG_USER | LOG_ERR, "Cannot insert to influxdb: %s\n", e.what());
-			break;
-		}
+        auto i = 0;
+        for (auto const & [id, value, now, name] : records) try
+        {
+            influx_.insert(name.data(), value, now);
+            ++i;
+        }
+        catch (std::exception const & e)
+        {
+            syslog(LOG_USER | LOG_ERR, "Cannot insert to influxdb: %s\n", e.what());
+            break;
+        }
 
-		if (i == 0) break;
+        if (i == 0) break;
 
-		sqlite_count_ = 0;
+        sqlite_count_ = 0;
 
-		try
-		{
-			sqlite_.delete_where_id_not_greater_than(std::get<0>(records[i - 1]));
-		}
-		catch (std::exception const & e)
-		{
-			syslog(LOG_USER | LOG_ERR, "Cannot delete from sqlite: %s\n", e.what());
-			break;
-		}
-	}
+        try
+        {
+            sqlite_.delete_where_id_not_greater_than(std::get<0>(records[i - 1]));
+        }
+        catch (std::exception const & e)
+        {
+            syslog(LOG_USER | LOG_ERR, "Cannot delete from sqlite: %s\n", e.what());
+            break;
+        }
+    }
 }
 
 inline void init_signal_handle()
 {
-	auto const handle = [](int){ s_running = false; };
-	std::signal(SIGTERM, handle);
-	std::signal(SIGINT, handle);
+    auto const handle = [](int){ s_running = false; };
+    std::signal(SIGTERM, handle);
+    std::signal(SIGINT, handle);
 }
 
 inline std::optional<int> w1_slave_read(const char * path)
 {
-	std::optional<int> ret;
+    std::optional<int> ret;
 
-	do
-	{
-		auto const destroyer = [](FILE * fp) { fclose(fp); };
-		std::unique_ptr<FILE, decltype(destroyer)> const fp{ fopen(path, "r"), destroyer };
-		if (!fp) break;
+    do
+    {
+        auto const destroyer = [](FILE * fp) { fclose(fp); };
+        std::unique_ptr<FILE, decltype(destroyer)> const fp{ fopen(path, "r"), destroyer };
+        if (!fp) break;
 
-		std::string_view line;
-		std::string_view const flag{ "t=" };
-		char buf[256], *endptr;
-		int len;
+        std::string_view line;
+        std::string_view const flag{ "t=" };
+        char buf[256], *endptr;
+        int len;
 
-		if (fscanf(fp.get(), "%[^\n]%n", buf, &len) != 1) break;
+        if (fscanf(fp.get(), "%[^\n]%n", buf, &len) != 1) break;
 
-		line = std::string_view{ buf, static_cast<size_t>(len) };
-		if (!line.ends_with("YES")) break;
+        line = std::string_view{ buf, static_cast<size_t>(len) };
+        if (!line.ends_with("YES")) break;
 
-		fgetc(fp.get()); // skip '\n'
+        fgetc(fp.get()); // skip '\n'
 
-		if (fscanf(fp.get(), "%[^\n]%n", buf, &len) != 1) break;
+        if (fscanf(fp.get(), "%[^\n]%n", buf, &len) != 1) break;
 
-		line = std::string_view{ buf, static_cast<size_t>(len) };
-		auto const pos = line.rfind(flag);
-		if (pos == std::string_view::npos) break;
+        line = std::string_view{ buf, static_cast<size_t>(len) };
+        auto const pos = line.rfind(flag);
+        if (pos == std::string_view::npos) break;
 
-		auto const t = line.substr(pos + flag.size());
-		auto const n = strtol(t.data(), &endptr, 10);
-		auto const ok =
-			endptr == t.end() &&
-			std::numeric_limits<int>::min() <= n &&
-			n <= std::numeric_limits<int>::max();
-		if (!ok) break;
-		ret = static_cast<int>(n);
-		return ret;
-	} while (false);
+        auto const t = line.substr(pos + flag.size());
+        auto const n = strtol(t.data(), &endptr, 10);
+        auto const ok =
+            endptr == t.end() &&
+            std::numeric_limits<int>::min() <= n &&
+            n <= std::numeric_limits<int>::max();
+        if (!ok) break;
+        ret = static_cast<int>(n);
+        return ret;
+    } while (false);
 
-	syslog(LOG_USER | LOG_ERR, "Cannot parse w1_slave\n");
-	return ret;
+    syslog(LOG_USER | LOG_ERR, "Cannot parse w1_slave\n");
+    return ret;
 }
 
 inline void w1_therm_run(storage_t & storage, const therm_config & config)
 {
-	syslog(LOG_USER | LOG_INFO, "w1_therm is started!\n");
+    syslog(LOG_USER | LOG_INFO, "w1_therm is started!\n");
 
-	s_running = true;
+    s_running = true;
 
-	std::chrono::minutes constexpr interval{ 5 };
-	std::chrono::steady_clock::time_point next_read_time;
+    std::chrono::minutes constexpr interval{ 5 };
+    std::chrono::steady_clock::time_point next_read_time;
 
-	while (s_running)
-	{
-		auto const now = std::chrono::steady_clock::now();
-		if (next_read_time <= now)
-		{
-			next_read_time = now + interval;
-			auto const therm = w1_slave_read(config.w1_slave_path_.c_str());
-			if likely(therm)
-			{
-				auto const utc_now = time(nullptr);
-				storage.insert(config.senor_name_.c_str(), *therm / double(1000), utc_now);
-			}
-		}
+    while (s_running)
+    {
+        auto const now = std::chrono::steady_clock::now();
+        if (next_read_time <= now)
+        {
+            next_read_time = now + interval;
+            auto const therm = w1_slave_read(config.w1_slave_path_.c_str());
+            if likely(therm)
+            {
+                auto const utc_now = time(nullptr);
+                storage.insert(config.senor_name_.c_str(), *therm / double(1000), utc_now);
+            }
+        }
 
-		std::this_thread::sleep_for(std::chrono::milliseconds{ 250 });
-	}
+        std::this_thread::sleep_for(std::chrono::milliseconds{ 250 });
+    }
 
-	syslog(LOG_USER | LOG_INFO, "w1_therm is stopped!\n");
+    syslog(LOG_USER | LOG_INFO, "w1_therm is stopped!\n");
 }
 
 inline storage_t init_storage(const therm_config & config)
 {
-	sqlite_storage sqlite{config.sqlite_db_.path_.c_str()};
-	influx_storage influx{config.influx_db_.host_,
-						  config.influx_db_.org_,
-						  config.influx_db_.bucket_,
-						  config.influx_db_.token_,
-						  "home",
-						  "temperature"};
-	syslog(LOG_USER | LOG_INFO, "sqlite3 and influxdb are initialized!\n");
-	return { std::move(sqlite), std::move(influx) };
+    sqlite_storage sqlite{config.sqlite_db_.path_.c_str()};
+    influx_storage influx{config.influx_db_.host_,
+                          config.influx_db_.org_,
+                          config.influx_db_.bucket_,
+                          config.influx_db_.token_,
+                          "home",
+                          "temperature"};
+    syslog(LOG_USER | LOG_INFO, "sqlite3 and influxdb are initialized!\n");
+    return { std::move(sqlite), std::move(influx) };
 }
 
 inline void init_influx_config(therm_config & config, const char * str)
 {
-	// valid settings: "host/org/bucket/token"
-	assert(str);
+    // valid settings: "host/org/bucket/token"
+    assert(str);
 
-	// set host
-	auto bgn = str;
-	auto end = strchr(bgn, '/');
-	if (!end)
-		throw std::invalid_argument{ "invalid db settings" };
-	config.influx_db_.host_.assign(bgn, static_cast<size_t>(end - bgn));
+    // set host
+    auto bgn = str;
+    auto end = strchr(bgn, '/');
+    if (!end)
+        throw std::invalid_argument{ "invalid db settings" };
+    config.influx_db_.host_.assign(bgn, static_cast<size_t>(end - bgn));
 
-	// set org
-	bgn = end + 1;
-	end = strchr(bgn, '/');
-	if (!end)
-		throw std::invalid_argument{ "invalid db settings" };
-	config.influx_db_.org_.assign(bgn, static_cast<size_t>(end - bgn));
+    // set org
+    bgn = end + 1;
+    end = strchr(bgn, '/');
+    if (!end)
+        throw std::invalid_argument{ "invalid db settings" };
+    config.influx_db_.org_.assign(bgn, static_cast<size_t>(end - bgn));
 
-	// set bucket
-	bgn = end + 1;
-	end = strchr(bgn, '/');
-	if (!end)
-		throw std::invalid_argument{ "invalid db settings" };
-	config.influx_db_.bucket_.assign(bgn, static_cast<size_t>(end - bgn));
+    // set bucket
+    bgn = end + 1;
+    end = strchr(bgn, '/');
+    if (!end)
+        throw std::invalid_argument{ "invalid db settings" };
+    config.influx_db_.bucket_.assign(bgn, static_cast<size_t>(end - bgn));
 
-	// set token
-	bgn = end + 1;
-	if (*bgn == 0)
-		throw std::invalid_argument{ "invalid db settings" };
-	config.influx_db_.token_.assign(bgn);
+    // set token
+    bgn = end + 1;
+    if (*bgn == 0)
+        throw std::invalid_argument{ "invalid db settings" };
+    config.influx_db_.token_.assign(bgn);
 }
 
 inline void load_config_file(therm_config & config, const char * path)
 {
-	// demo config file:
-	// sqlite w1_therm.db
-	// influx host/org/bucket/token
+    // demo config file:
+    // sqlite w1_therm.db
+    // influx host/org/bucket/token
 
-	assert(path);
-	auto const file_deleter = [](FILE * fp) { fclose(fp); };
-	std::unique_ptr<FILE, decltype(file_deleter)> fp{ fopen(path, "r"), file_deleter };
-	if (!fp)
-		throw std::runtime_error{ "Cannot open config file" };
+    assert(path);
+    auto const file_deleter = [](FILE * fp) { fclose(fp); };
+    std::unique_ptr<FILE, decltype(file_deleter)> fp{ fopen(path, "r"), file_deleter };
+    if (!fp)
+        throw std::runtime_error{ "Cannot open config file" };
 
-	char buf[256];
-	while (fgets(buf, sizeof buf, fp.get()))
-	{
-		auto const len = strlen(buf);
-		if (len == 0 || buf[len - 1] != '\n')
-			throw std::runtime_error{ "Invalid config file" };
-		buf[len - 1] = 0;
+    char buf[256];
+    while (fgets(buf, sizeof buf, fp.get()))
+    {
+        auto const len = strlen(buf);
+        if (len == 0 || buf[len - 1] != '\n')
+            throw std::runtime_error{ "Invalid config file" };
+        buf[len - 1] = 0;
 
-		if (strncmp(buf, "sqlite ", 7) == 0)
-			config.sqlite_db_.path_.assign(buf + 7);
-		else if (strncmp(buf, "influx ", 7) == 0)
-			init_influx_config(config, buf + 7);
-		else
-			throw std::runtime_error{ "Invalid config file" };
-	}
+        if (strncmp(buf, "sqlite ", 7) == 0)
+            config.sqlite_db_.path_.assign(buf + 7);
+        else if (strncmp(buf, "influx ", 7) == 0)
+            init_influx_config(config, buf + 7);
+        else
+            throw std::runtime_error{ "Invalid config file" };
+    }
 }
 
 inline therm_config parse_arguments(int const argc, char ** argv)
 {
-	therm_config config;
+    therm_config config;
 
-	try
-	{
-		if (argc < 3)
-			throw std::invalid_argument{ "too few argument" };
+    try
+    {
+        if (argc < 3)
+            throw std::invalid_argument{ "too few argument" };
 
-		constexpr auto * opts{ "p:n:d:c:" };
+        constexpr auto * opts{ "p:n:d:c:" };
 
-		for (int r; (r = getopt(argc, argv, opts)) != -1; )
-		{
-			switch(r)
-			{
-			case 'p':
-				config.w1_slave_path_ = optarg;
-				break;
+        for (int r; (r = getopt(argc, argv, opts)) != -1; )
+        {
+            switch(r)
+            {
+            case 'p':
+                config.w1_slave_path_ = optarg;
+                break;
 
-			case 'n':
-				config.senor_name_ = optarg;
-				break;
+            case 'n':
+                config.senor_name_ = optarg;
+                break;
 
-			case 'd':
-				config.daemonlize_ = true;
-				break;
+            case 'd':
+                config.daemonlize_ = true;
+                break;
 
-			case 'c':
-				load_config_file(config, optarg);
-				break;
+            case 'c':
+                load_config_file(config, optarg);
+                break;
 
-			default:
-				throw std::invalid_argument{ "invalid argument" };
-			}
-		}
+            default:
+                throw std::invalid_argument{ "invalid argument" };
+            }
+        }
 
-		if (config.senor_name_.empty() || config.senor_name_.empty())
-			throw std::invalid_argument{ "invalid argument" };
-	}
-	catch (std::invalid_argument const &)
-	{
-		std::cerr
-			<< "usage: " << argv[0] << " [options] -p <path> -n <name>" << std::endl
-			<< '\t' << "-p <path>" << '\t' << "Set the w1_slave path" << std::endl
-			<< '\t' << "-n <name>" << '\t' << "Set the senor name" << std::endl
-			<< '\t' << "-d       " << '\t' << "daemonlize if set" << std::endl;
-		exit(EXIT_FAILURE);
-	}
+        if (config.senor_name_.empty() || config.senor_name_.empty())
+            throw std::invalid_argument{ "invalid argument" };
+    }
+    catch (std::invalid_argument const &)
+    {
+        std::cerr
+            << "usage: " << argv[0] << " [options] -p <path> -n <name>" << std::endl
+            << '\t' << "-p <path>" << '\t' << "Set the w1_slave path" << std::endl
+            << '\t' << "-n <name>" << '\t' << "Set the senor name" << std::endl
+            << '\t' << "-d       " << '\t' << "daemonlize if set" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
-	return config;
+    return config;
 }
 
 inline void init_log(const char * arg0)
 {
-	std::string_view tmp{ arg0 };
-	auto const pos = tmp.rfind('/');
-	if (pos != std::string_view::npos)
-		tmp = tmp.substr(pos + 1);
-	openlog(tmp.data(), LOG_PERROR | LOG_PID | LOG_NDELAY, LOG_USER);
+    std::string_view tmp{ arg0 };
+    auto const pos = tmp.rfind('/');
+    if (pos != std::string_view::npos)
+        tmp = tmp.substr(pos + 1);
+    openlog(tmp.data(), LOG_PERROR | LOG_PID | LOG_NDELAY, LOG_USER);
 }
 
 inline void deinit_log()
 {
-	closelog();
+    closelog();
 }
 
 inline void daemonlize()
 {
-	if (daemon(1, 0))
-	{
-		syslog(LOG_USER | LOG_ERR, "Cannot daemonlize\n");
-		exit(EXIT_FAILURE);
-	}
+    if (daemon(1, 0))
+    {
+        syslog(LOG_USER | LOG_ERR, "Cannot daemonlize\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 int main(int argc, char ** argv)
 {
-	auto const config = parse_arguments(argc, argv);
+    auto const config = parse_arguments(argc, argv);
 
-	if (config.daemonlize_)
-		daemonlize();
+    if (config.daemonlize_)
+        daemonlize();
 
-	init_log(argv[0]);
+    init_log(argv[0]);
 
-	init_signal_handle();
+    init_signal_handle();
 
-	auto storage = init_storage(config);
+    auto storage = init_storage(config);
 
-	w1_therm_run(storage, config);
+    w1_therm_run(storage, config);
 
-	deinit_log();
+    deinit_log();
 }
